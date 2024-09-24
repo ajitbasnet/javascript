ee896513e3cc04e1b395325a725bb9fed9e4f4f1
ajitbasnet <152585876+ajitbasnet@users.noreply.github.com>
2024-09-24 08:54:26 +0000

    variables done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144556
+let accountEmail = "ajit@google.com"
+var accountPassword = "12345"
+accountCity = "Sarlahi"
+let accountState;
+// accountId = 2
+accountEmail = "hc@gmail.com"
+accountPassword = "5678"
+accountCity = "india"
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
