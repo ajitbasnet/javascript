1112e0a668758e2f953e611dbc8efb973d116a86
ajitbasnet <152585876+ajitbasnet@users.noreply.github.com>
2024-09-24 09:14:56 +0000

    discuss datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS CODE as newer version
+
+// alert("hello")
+//alert(3 + 3)// we are using nodejs, not brower
+
+let name ="ajit"
+let age = 20
+let isLoggedIn = false
+
+// number range =>2 to power 53
+//bigint for bigger numbers
+// string=> ""
+//boolean=> true/false
+// null=> stand alone value
+// undefined => value not assigned
+// symbol=> for uniqueness ko lagi
+
+// object
+
+console.log(typeof undefined);
